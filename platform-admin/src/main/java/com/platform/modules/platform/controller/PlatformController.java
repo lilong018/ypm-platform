8e820fe477f7af3d64abc3b95ebdb25fd0295098
lilong <abing18ll@163.com>
2020-08-05 11:38:06 +0800

    平台相关内容

--- platform-admin/src/main/java/com/platform/modules/platform/controller/PlatformController.java
+++ platform-admin/src/main/java/com/platform/modules/platform/controller/PlatformController.java
@@ -3,7 +3,7 @@ package com.platform.modules.platform.controller;
 import java.util.Arrays;
 import java.util.Map;
 
-import ValidatorUtils;
+import com.platform.common.validator.ValidatorUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
