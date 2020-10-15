30c209c89f5adb5636b69cee9106e7616fb3db0e
lilong018 <abing18ll@163.com>
2020-10-15 09:05:51 +0800

    员工注册添加权限

--- platform-admin/src/main/java/com/platform/modules/employee/service/impl/EmployeeServiceImpl.java
+++ platform-admin/src/main/java/com/platform/modules/employee/service/impl/EmployeeServiceImpl.java
@@ -103,15 +103,76 @@ public class EmployeeServiceImpl<main> extends ServiceImpl<EmployeeDao, Employee
      * 赋予权限
      * @param args
      */
+
     public static void main(String[] args) {
+        {
 
 
 //        5f6ef1186c1b725440329897
-        Map<String, String> headerMap = new HashMap<String, String>();
-        Map<String, Object> params = new HashMap<String, Object>();
-        // TODO: 2020/9/22 修改人员增加接口
-        headerMap.put("x-auth-token", AuthService.getPlatformManager());
+            Map<String, String> headerMap = new HashMap<String, String>();
+            Map<String, Object> params = new HashMap<String, Object>();
+            // TODO: 2020/9/22 修改人员增加接口
+            headerMap.put("x-auth-token", AuthService.getPlatformManager());
 //        /添加用户权限
+            Map<String, Object> privilegeParams = new HashMap<String, Object>();
+
+            List<Map<String, Object>> userPrivileges = new ArrayList<Map<String, Object>>();
+
+            Map<String, Object> platformMap = new HashMap<String, Object>();
+
+            List<Map<String, Object>> privilegesList = new ArrayList<Map<String, Object>>();
+            Map<String, Object> privilegesMap = new HashMap<String, Object>();
+            privilegesMap.put("category",1);
+            privilegesMap.put("privilege",15);
+            privilegesList.add(privilegesMap);
+            platformMap.put("platformId",AuthService.getPlatformId());
+            platformMap.put("privileges",privilegesList);
+            userPrivileges.add(platformMap);
+
+            privilegeParams.put("employeeId","5f6ef1186c1b725440329897");
+
+            privilegeParams.put("userPrivileges",userPrivileges);
+
+            try {
+                String privilegesRes = HttpUtil.post(UrlConstans.BASEURL + UrlConstans.PRIVILEGES, headerMap, JSON.toJSONString(privilegeParams));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("page",0);
+        params.put("limit",10);
+        Integer pageNumber = 0;
+        Integer pageSize = 10;
+        Map<String, String> headerMap = new HashMap<String, String>();
+        Map<String, String> urlParams = ParamsUtils.convertParams(params);
+        // TODO: 2020/9/14 使用authserve获取platformId
+        urlParams.put("platformId",AuthService.getPlatformId());
+        headerMap.put("x-auth-token", AuthService.getPlatformManager());
+        PageUtils page = null;
+        try {
+            String res = HttpUtil.get(UrlConstans.BASEURL + UrlConstans.EMPLOYEES, headerMap, urlParams, null);
+            System.out.println(res);
+            EmployeeRespond employeeRespond = JSON.parseObject(res, EmployeeRespond.class);
+            List<EmployeeResults> employeeResults = employeeRespond.getPayload().getResults();
+            List<Employee> employees = new ArrayList<Employee>();
+            for (EmployeeResults employeeResult : employeeResults) {
+                Employee employee = employeeResult.getEmployee();
+                employee.setPlatformId(employeeResult.getPlatform().getId());
+                employee.setPlatformName(employeeResult.getPlatform().getName());
+                employees.add(employee);
+            }
+            page = new PageUtils(employees, employeeRespond.getPayload().getTotal(), pageSize, pageNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addPlv(EmployeeEntity employee) throws Exception{
+        Map<String, String> headerMap = new HashMap<String, String>();
+        headerMap.put("x-auth-token", AuthService.getAdminToken());
+//        String employeeId = userPayload.get("employeeId");
+
         Map<String, Object> privilegeParams = new HashMap<String, Object>();
 
         List<Map<String, Object>> userPrivileges = new ArrayList<Map<String, Object>>();
@@ -120,22 +181,18 @@ public class EmployeeServiceImpl<main> extends ServiceImpl<EmployeeDao, Employee
 
         List<Map<String, Object>> privilegesList = new ArrayList<Map<String, Object>>();
         Map<String, Object> privilegesMap = new HashMap<String, Object>();
-        privilegesMap.put("category",1);
+        privilegesMap.put("category",3);
         privilegesMap.put("privilege",15);
         privilegesList.add(privilegesMap);
-        platformMap.put("platformId",AuthService.getPlatformId());
+//        platformMap.put("platformId",platformId);
         platformMap.put("privileges",privilegesList);
         userPrivileges.add(platformMap);
 
-        privilegeParams.put("employeeId","5f6ef1186c1b725440329897");
+//        privilegeParams.put("employeeId",employeeId);
 
         privilegeParams.put("userPrivileges",userPrivileges);
 
-        try {
-            String privilegesRes = HttpUtil.post(UrlConstans.BASEURL + UrlConstans.PRIVILEGES, headerMap, JSON.toJSONString(privilegeParams));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String privilegesRes = HttpUtil.post(UrlConstans.BASEURL + UrlConstans.PRIVILEGES, headerMap, JSON.toJSONString(privilegeParams));
     }
 
     @Override
